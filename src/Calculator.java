3cdfd7862d35920f307a6f84c295f64ee2ab6f02
Parv Arora <arorap2@student.douglascollege.ca>
2020-05-22 08:37:55 -0700

    Calculator class updated

---
+++ src/Calculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author parva
+ *
+ */
+public class Calculator {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int varA,varB;
+	int result;
+	System.out.println("Enter variable A");
+	varA=sc.nextInt();
+	System.out.println("Enter variable B");
+	varB=sc.nextInt();
+	result=varA+varB;
+	System.out.println("The sum is :"+result);
+	
+}
+}
